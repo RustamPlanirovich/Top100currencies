b0d4e4a7a5cfb4dc3dee05fc0b08a482e52529d9
RustamPlanirovich <israr.ru@yandex.ru>
2021-10-05 05:40:33 +0700

    - добавил CurrenciesListFragment.kt - изменил fragment_currencies_list.xml

---
+++ app/src/main/java/com/nauk0a/top100currencies/fragments/CurrenciesListFragment.kt
@@ -0,0 +1,22 @@
+package com.nauk0a.top100currencies.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nauk0a.top100currencies.R
+
+
+class CurrenciesListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_currencies_list, container, false)
+    }
+
+}
