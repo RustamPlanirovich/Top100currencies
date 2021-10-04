0d27f64094035a801685a07bf5f8483b694b515e
RustamPlanirovich <israr.ru@yandex.ru>
2021-10-05 04:44:09 +0700

    Initial commit

---
+++ app/src/main/java/com/nauk0a/top100currencies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nauk0a.top100currencies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
