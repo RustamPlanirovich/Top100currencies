4bb95196ec4550e870748a2c78131e9a9bbc6475
RustamPlanirovich <israr.ru@yandex.ru>
2021-10-06 00:23:34 +0700

    - добавил Ext.kt - добавил CurrenciesContract.kt - добавил YearValueFormat.kt - добавил BaseContract.kt

---
+++ app/src/main/java/com/nauk0a/top100currencies/YearValueFormat.kt
@@ -0,0 +1,17 @@
+package com.nauk0a.top100currencies
+
+import java.util.*
+
+class YearValueFormat : AxisValueFormatter {
+
+    override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = value.toLong()
+        return calendar.toFormatted()
+    }
+
+    fun Calendar.toFormatted(): String {
+        val date = this.timeInMillis
+        return date.dateToString("MMM yyyy")
+    }
+}
---
+++ app/src/main/java/com/nauk0a/top100currencies/contract/CurrenciesContract.kt
@@ -0,0 +1,19 @@
+package com.nauk0a.top100currencies.contract
+
+import com.nauk0a.top100currencies.adapter.CurrenciesAdapter
+
+class CurrenciesContract {
+    interface View : BaseContract.View {
+        fun addCurrency(currency: CurrenciesAdapter.Currency)
+        fun notifyAdapter()
+        fun showProgress()
+        fun hideProgress()
+        fun showErrorMessage(error: String?)
+        fun refresh()
+    }
+
+    abstract class Presenter : BaseContract.Presenter<View>() {
+        abstract fun makeList()
+        abstract fun refreshList()
+    }
+}
