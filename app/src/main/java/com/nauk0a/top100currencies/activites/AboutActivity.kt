72259c5a63952cbfc50256192114806f6d4235de
RustamPlanirovich <israr.ru@yandex.ru>
2021-10-05 23:49:14 +0700

    - реализовал даггер - создал макет итема ресайклер вью - создал классф ретрофита

--- app/src/main/java/com/nauk0a/top100currencies/activites/AboutActivity.kt
+++ app/src/main/java/com/nauk0a/top100currencies/activites/AboutActivity.kt
@@ -1,7 +1,10 @@
 package com.nauk0a.top100currencies.activites
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.nauk0a.top100currencies.R
 
 class AboutActivity : AppCompatActivity() {
@@ -10,5 +13,11 @@ class AboutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_about)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val button = findViewById<Button>(R.id.buttomRateApp)
+
+        button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?" +
+                    "id=${applicationContext.packageName}")))
+        }
     }
 }
