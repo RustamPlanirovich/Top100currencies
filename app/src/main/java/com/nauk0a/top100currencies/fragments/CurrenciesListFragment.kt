068f98317ab8548d3a65525c60126682ca6e7019
RustamPlanirovich <israr.ru@yandex.ru>
2021-10-05 20:53:54 +0700

    -

--- app/src/main/java/com/nauk0a/top100currencies/fragments/CurrenciesListFragment.kt
+++ app/src/main/java/com/nauk0a/top100currencies/fragments/CurrenciesListFragment.kt
@@ -15,7 +15,7 @@ class CurrenciesListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_currencies_list, container, false)
     }
 
